54bc65424b593309ba52b10dad387a6b777e961c
Sawon Bhattacharya <35263795+SawonBhattacharya@users.noreply.github.com>
2020-10-30 12:41:56 +0530

    Update Deli

--- Deli.cpp
+++ Deli.cpp
@@ -6,49 +6,44 @@
 #include<string>
 //#include"Scale.cpp"
 //#include"Price_list.cpp"
-#include"Cart.cpp"
+//#include"Cart.cpp"
 
 using namespace std;
 class deli{
 private:
-    Cart cart;
-    static queue <int> customer;
-    static int customer_count,customer_id;
-public:
-    deli(){
-        customer_id=(++customer_count)%11;
-        }
+    //Cart cart;
 
-    int get_customer_id(){
+    int customer_count=0,customer_id=0;
+public:
+
+    int arrival(){
+        customer_id=(++customer_count)%11;
         return customer_id;
     }
-    void static arrival(int id){
-        if(customer.size()<10){
-            customer.push(id);
-        }
-        else{
-            cout<<"Queue is Full"<<endl;
-        }
-    }
     void check_out_customer(){
-        int check_out_id=customer.front();
-        cout<<"Thank you customer no: "<<check_out_id;
-        customer_count--;
-        customer.pop();
+       // int check_out_id=customer.front();
+        //cout<<"Thank you customer no: "<<check_out_id;
+       // customer_count--;
+        //customer.pop();
 
     }
    ~deli(){
        cout<<"Start of a New Day!!!";
    }
 };
-int deli::customer_count,deli::customer_id;
-queue <int> deli:: customer;
+
+
 int main(){
-	
-    deli cust[10];
-    int c_id;
-    for(int i = 0; i < 10; i++) {
-    	c_id = cust[i].get_customer_id();
-    	deli::arrival(c_id);
-	}
+
+    deli cust;
+    queue <int> cust_det;
+    while(cust_det.size()<=9){
+    	cust_det.push(cust.arrival());
+
+    }
+    while(cust_det.size()>0){
+        cout<<"Element:"<<cust_det.front()<<endl;
+        cust_det.pop();
+    }
+
 }
