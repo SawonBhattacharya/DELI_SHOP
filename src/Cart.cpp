b229fb62ef0dd5f97d943a9e32d94c74bf3a6068
Rajarsi Saha <rajarsi3997@gmail.com>
2020-10-31 16:13:28 +0530

    FINAL UPDATES FROM MY SIDE

--- src/Cart.cpp
+++ src/Cart.cpp
@@ -65,10 +65,12 @@ class Cart {
         //print invoice so far (for packages in the cart in the order they were inserted)
         void print_invoice() {
             map<string, double>::iterator itr;
-            cout << "PRODUCT\t\tPRICE" << endl;
-            cout << "-------\t\t------" << endl;
+            cout << "PRODUCT\t\tQUANTITY\tRATE\t\tPRICE" << endl;
+            cout << "-------\t\t--------\t----\t\t-----" << endl;
+            if(cart.begin() == cart.end()) 
+            	cout << "Cart \t is \t empty" << endl;
             for(itr = cart.begin(); itr != cart.end(); ++itr) {
-                cout << itr->first << "\t\t" << itr->second/list.get_price(itr->first) << "\t\t" << itr->second << endl;
+                cout << itr->first << "\t    " << itr->second/list.get_price(itr->first) << "\t\t" << list.get_price(itr->first) << "\t\t" << itr->second << endl;
             }
             cout << "\n";
         }
