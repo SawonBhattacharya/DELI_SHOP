3306bdea535cf7acda1e64ed26ff6e65ae4e29c5
Rajarsi Saha <rajarsi3997@gmail.com>
2020-11-02 23:45:40 +0530

    FINAL RELEASE UPLOAD

---
+++ src/build/CMakeFiles/DELI_SHOP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DELI_SHOP.dir/Main.cpp.obj"
+  "DELI_SHOP.exe"
+  "DELI_SHOP.exe.manifest"
+  "DELI_SHOP.pdb"
+  "libDELI_SHOP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DELI_SHOP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/build/CMakeFiles/DELI_SHOP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DELI_SHOP.
+# This may be replaced when dependencies are built.
